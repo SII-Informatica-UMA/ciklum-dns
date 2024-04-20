2477965942bdf2ca9952c0064881b546a4730fad
Daniel Sánchez Triviño <danielsancheztrivino@uma.es>
2024-04-20 10:40:41 +0200

    Repositorios con métodos adicionales

--- jpa/src/main/java/es/uma/sisinfparInt/dns/jpa/repositories/EntrenadorRepository.java
+++ jpa/src/main/java/es/uma/sisinfparInt/dns/jpa/repositories/EntrenadorRepository.java
@@ -1,9 +1,22 @@
 package es.uma.sisinfparInt.dns.jpa.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import es.uma.sisinfparInt.dns.jpa.entities.Entrenador;
 
 public interface EntrenadorRepository extends JpaRepository<Entrenador,Integer> {
-    //TO-DO//
+    Optional<Entrenador> findById(Integer id); //Busca un entrenador
+
+    List<Entrenador> findAll(); //Busca todos los entrenadores
+
+    Entrenador save(Entrenador entrenador); //Guarda o actualiza un entrenador
+
+    List<Entrenador> saveAll(); //Guarda o actualiza todos los entrenadores
+
+    void deleteById(Integer id); //Borra un entrenador
+
+    void deleteAll(); //Borra todos los entrenadores
 }
