dd0737ecaebd87b296b2e8a56cda9ed36a9330ed
danicl06 <116908612+danicl06@users.noreply.github.com>
2024-05-12 17:13:29 +0200

    Pruebas dietas/id
    pruebas de /dietas/id para delete,put,get salvo para error 403

--- dietasUsuarios-ciklum-backend/src/test/java/es/uma/dns/dietasUsuariosCiklumBackend/FitnessGestionDietasAsignacionUsuariosBackendApplicationTests.java
+++ dietasUsuarios-ciklum-backend/src/test/java/es/uma/dns/dietasUsuariosCiklumBackend/FitnessGestionDietasAsignacionUsuariosBackendApplicationTests.java
@@ -1,8 +1,15 @@
 package es.uma.dns.dietasUsuariosCiklumBackend;
 
+import es.uma.dns.dietasUsuariosCiklumBackend.dtos.DietaDTO;
+import es.uma.dns.dietasUsuariosCiklumBackend.dtos.UsuarioDTO;
+import es.uma.dns.dietasUsuariosCiklumBackend.entities.Cliente;
 import es.uma.dns.dietasUsuariosCiklumBackend.entities.Dieta;
 import es.uma.dns.dietasUsuariosCiklumBackend.entities.Entrenador;
+import es.uma.dns.dietasUsuariosCiklumBackend.entities.Sex;
+import es.uma.dns.dietasUsuariosCiklumBackend.entities.Usuario;
+import es.uma.dns.dietasUsuariosCiklumBackend.repositories.ClienteRepository;
 import es.uma.dns.dietasUsuariosCiklumBackend.repositories.DietaRepository;
+import es.uma.dns.dietasUsuariosCiklumBackend.repositories.EntrenadorRepository;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -12,6 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.test.annotation.DirtiesContext;
@@ -38,6 +46,12 @@ class FitnessGestionDietasAsignacionUsuariosBackendApplicationTests {
 
 	@Autowired
 	private DietaRepository dietaRepo;
+	@Autowired
+	private EntrenadorRepository entrenadorRepo;
+
+	@Autowired
+	private ClienteRepository clienteRepo;
+	
 
 	private URI uri(String scheme, String host, int port, String ...paths) {
 		UriBuilderFactory ubf = new DefaultUriBuilderFactory();
@@ -113,8 +127,61 @@ class FitnessGestionDietasAsignacionUsuariosBackendApplicationTests {
 
 		@BeforeEach
 		public void insertarDatos(){
+			
+			//Entrenador 1
+			Date a=new Date(111111);
+			var entrenador1= new Entrenador();
+			entrenador1.setId(1L);
+			entrenador1.setAdministrador(false);
+			entrenador1.setApellido1("carr");
+			entrenador1.setApellido2("carr");
+			entrenador1.setEmail("ejemplo@gmail.com");
+			entrenador1.setNombre("Luis");
+			entrenador1.setPassword("122");
+			entrenador1.setFechaNacimiento(a);
+			entrenador1.setFechaAlta(a);
+			entrenador1.setFechaBaja(a);
+			entrenador1.setTelefono("111199999");
+			entrenador1.setDireccion("calle ejemplo 26");
+			entrenador1.setDni("00000000A");
+			entrenador1.setEspecialidad("brazos");
+			entrenador1.setTitulacion("ninguna");
+			entrenador1.setExperiencia("12");
+			entrenador1.setObservaciones("muy ruidoso");
+			entrenador1.setIdCentro(1L);
+
+			entrenadorRepo.save(entrenador1);
+
+			//Entrenador 2
+			Date b=new Date(222222);
+			var entrenador2= new Entrenador();
+			entrenador1.setId(2L);
+			entrenador1.setAdministrador(false);
+			entrenador1.setApellido1("call");
+			entrenador1.setApellido2("call");
+			entrenador1.setEmail("ejemplo2@gmail.com");
+			entrenador1.setNombre("Pepe");
+			entrenador1.setPassword("123");
+			entrenador1.setFechaNacimiento(b);
+			entrenador1.setFechaAlta(b);
+			entrenador1.setFechaBaja(b);
+			entrenador1.setTelefono("444499999");
+			entrenador1.setDireccion("calle ejemplo2 36");
+			entrenador1.setDni("00000001A");
+			entrenador1.setEspecialidad("piernas");
+			entrenador1.setTitulacion("podología");
+			entrenador1.setExperiencia("1");
+			entrenador1.setObservaciones("muy energico");
+			entrenador1.setIdCentro(1L);
+
+			entrenadorRepo.save(entrenador2);
+			
+			
+			
+			
 			// Dieta 1
 			var dieta1 = new Dieta();
+			dieta1.setId(1L);
 			dieta1.setNombre("Mediterranea");
 			dieta1.setDescripcion("Comer verdura con aceite de oliva y pescado");
 			dieta1.setObservaciones("eso");
@@ -126,10 +193,10 @@ class FitnessGestionDietasAsignacionUsuariosBackendApplicationTests {
 			alimentos.add("Pescado");
 			dieta1.setAlimentos(alimentos);
 			// VER COMO COGER UN ENTRENADOR-> id=1L
-			// dieta1.setEntrenador();
+			dieta1.setEntrenador(entrenador1);
+			dietaRepo.save(dieta1);
 
 			// VER COMO COGER UN CLIENTE
-			dietaRepo.save(dieta1);
 
 			// Dieta 2 con mismo entrenador creado que dieta1
 			var dieta2 = new Dieta();
@@ -160,10 +227,182 @@ class FitnessGestionDietasAsignacionUsuariosBackendApplicationTests {
 			alimentos3.add("Todo");
 			dieta3.setAlimentos(alimentos3);
 			// VER COMO COGER UN ENTRENADOR-> id=2L
-			// dieta2.setEntrenador();
+			dieta2.setEntrenador(entrenador1);
 
 			// VER COMO COGER UN CLIENTE
 			dietaRepo.save(dieta3);
+
+
+			//cliente 1
+			
+			var cliente1= new Cliente();
+			cliente1.setId(3L);
+			cliente1.setNombre("Juan");
+			cliente1.setApellido1("ap");
+			cliente1.setApellido2("ap2");
+			cliente1.setAdministrador(false);
+			cliente1.setDieta(dieta1);
+			cliente1.setEmail("ejemplocli@gmail.com");
+			cliente1.setFechaNacimiento(b);
+			cliente1.setPassword("111");
+			cliente1.setTelefono("222244442");
+			cliente1.setDireccion("calle ejemplo 35");
+			cliente1.setDni("22222222G");
+			cliente1.setSexo(Sex.HOMBRE);
+			clienteRepo.save(cliente1);
+
+			//cliente 2
+			
+			var cliente2= new Cliente();
+			cliente1.setId(4L);
+			cliente1.setNombre("Juana");
+			cliente1.setApellido1("ape");
+			cliente1.setApellido2("ape2");
+			cliente1.setAdministrador(false);
+			cliente1.setDieta(dieta2);
+			cliente1.setEmail("ejemplocliente@gmail.com");
+			cliente1.setFechaNacimiento(a);
+			cliente1.setPassword("112");
+			cliente1.setTelefono("222245442");
+			cliente1.setDireccion("calle ejemplo 55");
+			cliente1.setDni("22222223F");
+			cliente1.setSexo(Sex.MUJER);
+			clienteRepo.save(cliente2);
+			
+		}
+
+		
+		@Nested
+		@DisplayName("al consultar una dieta en concreto")
+		public class ObtenerDietas {
+			@Test
+			@DisplayName("devuelve la dieta cuando existe")
+			public void devuelveDieta() {
+				var peticion = get("http", "localhost",port, "/dieta/1");
+				
+				var respuesta = restTemplate.exchange(peticion, DietaDTO.class);
+				
+				assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
+				assertThat(respuesta.hasBody()).isTrue();
+				assertThat(respuesta.getBody()).isNotNull();
+			}
+			
+			@Test
+			@DisplayName("da error cuando no existe la dieta")
+			public void errorCuandoDietaNoExiste() {
+				var peticion = get("http", "localhost",port, "/dieta/47");
+				
+				var respuesta = restTemplate.exchange(peticion,
+						new ParameterizedTypeReference<List<DietaDTO>>() {});
+				
+				assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
+				assertThat(respuesta.hasBody()).isEqualTo(false);
+			}
+
+			@Test
+			@DisplayName("da error cuando no se realiza una bad request")
+			public void errorCuandoBadRequest() {
+				var peticion = get("http", "localhost",port, "/dietasss/47");
+				
+				var respuesta = restTemplate.exchange(peticion,
+						new ParameterizedTypeReference<List<DietaDTO>>() {});
+				
+				assertThat(respuesta.getStatusCode().value()).isEqualTo(400);
+				assertThat(respuesta.hasBody()).isEqualTo(false);
+			}
+
+			//FALTA EL 403 : ACCESO NO AUTORIZADO
+		}
+
+		@Nested
+            @DisplayName("al actualizar una dieta")
+            public class ActualizaDieta {
+                @Test
+                @DisplayName("actualiza correctamente si la dieta existe")
+                @DirtiesContext
+                public void actualizaCorrectamente() {
+                    var dieta = Dieta.builder()
+                            .nombre("Malagueña")
+                            .build();
+
+                    var peticion = put("http", "localhost",port, "/dieta/1", dieta);
+
+                    var respuesta = restTemplate.exchange(peticion,Dieta.class);
+
+                    assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
+                    Dieta dietalmacenada = dietaRepo.findById(1L).get();
+                    assertThat(dietalmacenada.getNombre()).isEqualTo(dieta.getNombre());
+                }
+                @Test
+                @DisplayName("da error cuando la dieta no existe")
+                public void errorCuandoDietaNoExiste() {
+                    var dieta = Dieta.builder()
+                            .nombre("Inexistente")
+                            .build();
+                    var peticion = put("http", "localhost",port, "/dieta/50", dieta);
+
+                    var respuesta = restTemplate.exchange(peticion,Void.class);
+
+                    assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
+                    assertThat(respuesta.hasBody()).isEqualTo(false);
+                }
+
+				@Test
+                @DisplayName("da error cuando se realiza bad request")
+                public void errorCuandoBadRequest() {
+                    var dieta = Dieta.builder()
+                            .nombre("Bad Request")
+                            .build();
+                    var peticion = put("http", "localhost",port, "/dietasss/50", dieta);
+
+                    var respuesta = restTemplate.exchange(peticion,Void.class);
+
+                    assertThat(respuesta.getStatusCode().value()).isEqualTo(400);
+                    assertThat(respuesta.hasBody()).isEqualTo(false);
+                }
+
+				//FALTA EL 403 : ACCESO NO AUTORIZADO
+            }
+
+		@Nested
+		@DisplayName("al eliminar una dieta")
+		public class EliminarDietas {
+			@Test
+			@DisplayName("la elimina cuando existe")
+			public void eliminarDietaCorrectamente() {
+				var peticion = delete("http", "localhost",port, "/dieta/1");
+				
+				var respuesta = restTemplate.exchange(peticion,Void.class);
+				
+				assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
+				List<Dieta> dietas = dietaRepo.findAll();
+				assertThat(dietas).hasSize(2);
+				assertThat(dietas).allMatch(c->c.getId()!=1);
+			}
+			
+			@Test
+			@DisplayName("da error cuando la dieta no existe")
+			public void errorCuandoDietaNoExiste() {
+				var peticion = delete("http", "localhost",port, "/dieta/50");
+				
+				var respuesta = restTemplate.exchange(peticion,Void.class);
+				
+				assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
+				assertThat(respuesta.hasBody()).isEqualTo(false);
+			}
+
+			@Test
+			@DisplayName("da error cuando se hace una bad request")
+			public void errorCuandoBadRequest() {
+				var peticion = delete("http", "localhost",port, "/dietasss/35");
+
+				var respuesta = restTemplate.exchange(peticion,Void.class);
+
+				assertThat(respuesta.getStatusCode().value()).isEqualTo(400);
+				assertThat(respuesta.hasBody()).isEqualTo(false);
+			}
+
+			//FALTA EL 403 : ACCESO NO AUTORIZADO
 		}
 	}
 
