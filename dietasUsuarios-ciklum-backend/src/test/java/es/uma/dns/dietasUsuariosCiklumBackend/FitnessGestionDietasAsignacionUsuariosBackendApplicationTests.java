18732595a4a2d860d079251f6ffb9fe2b7409fcb
NoeliaCarGon <0610919334@uma.es>
2024-05-31 18:27:44 +0200

    Agregado reinicio bd + Realizacion mockitos
    Ejemplos de mockitos finalizados.
    BeforeEach borrar base de datos incluido

--- dietasUsuarios-ciklum-backend/src/test/java/es/uma/dns/dietasUsuariosCiklumBackend/FitnessGestionDietasAsignacionUsuariosBackendApplicationTests.java
+++ dietasUsuarios-ciklum-backend/src/test/java/es/uma/dns/dietasUsuariosCiklumBackend/FitnessGestionDietasAsignacionUsuariosBackendApplicationTests.java
@@ -220,6 +220,11 @@ class FitnessGestionDietasAsignacionUsuariosBackendApplicationTests {
 	public void init() {
 		mockServer = MockRestServiceServer.createServer(mockitoRestTemplate);
 	}
+
+	@BeforeEach
+	public void initializeDatabase() {
+		dietaRepo.deleteAll();
+	}
 	//--------------------------------------------------------------------------------------------------------------
 
 	//==================================================================================================================
@@ -244,26 +249,7 @@ class FitnessGestionDietasAsignacionUsuariosBackendApplicationTests {
 			var peticion = get("http", "localhost", port, "/dieta",
 					true, Long.toString(1L),entrenador1());
 
-			// Creamos la peticion para el mock
-			String tokenServidor = seguridad.generateToken("150");
-			//---------------------------------MOCKITO------------------------------------------------------------------
-			var entrenador = EntrenadorDTO.builder()
-					.idUsuario(1L)
-					.build();
 
-			ObjectMapper objectMapper = new ObjectMapper();
-			String entrenadorJson = objectMapper.writeValueAsString(entrenador);
-
-			mockServer.expect(ExpectedCount.once(),
-					requestTo(uri("http", "localhost", portExterno, "/entrenador/1")))
-					.andExpect(method(HttpMethod.GET))
-					.andExpect(header("Authorization","Bearer " + tokenServidor))
-					.andRespond(withStatus(HttpStatus.OK)
-					.contentType(APPLICATION_JSON)
-					.body(entrenadorJson));
-
-
-			//----------------------------------------------------------------------------------------------------------
 			var respuesta = restTemplate.exchange(peticion,
 					new ParameterizedTypeReference<List<DietaDTO>>() {
 					});
@@ -293,10 +279,13 @@ class FitnessGestionDietasAsignacionUsuariosBackendApplicationTests {
 			String clienteJson = objectMapper.writeValueAsString(cliente);
 
 			var peticionExterna = "http://localhost:" + portExterno + "/cliente/4";
-			mockServer.expect(requestTo(peticionExterna))
+			mockServer.expect(ExpectedCount.twice(),
+							requestTo(uri("http", "localhost", portExterno, "/cliente/4")))
 							.andExpect(method(HttpMethod.GET))
-							.andExpect(header("Authorization","Bearer " + tokenServidor))
-							.andRespond(withSuccess(clienteJson, APPLICATION_JSON));
+							.andExpect(header("Authorization","Bearer " + DietaServicio.token))
+							.andRespond(withStatus(HttpStatus.OK)
+							.contentType(APPLICATION_JSON)
+							.body(clienteJson));
 
 
 			//----------------------------------------------------------------------------------------------------------
@@ -310,7 +299,7 @@ class FitnessGestionDietasAsignacionUsuariosBackendApplicationTests {
 
 			mockServer.verify();
 			assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
-			assertThat(respuesta.getBody()).isEmpty();
+			assertThat(respuesta.getBody()).isEqualTo(new ArrayList<>());
 		}
 
 		/*
@@ -537,7 +526,7 @@ class FitnessGestionDietasAsignacionUsuariosBackendApplicationTests {
 	@Nested
 	@DisplayName("cuando la base de datos esté llena...")
 	public class BaseDatosDietasLlena{
-		
+
 		@BeforeEach
 		public void insertarDatos(){
 			
